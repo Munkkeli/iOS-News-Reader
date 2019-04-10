7252e221dee65f78436a91f21fafb6b45461c130
Tuomas Pöyry <tuomas@munkkeli.io>
2019-04-10 09:58:12 +0300

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Tuomas Pöyry on 10/04/2019.
+//  Copyright © 2019 Tuomas Pöyry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
